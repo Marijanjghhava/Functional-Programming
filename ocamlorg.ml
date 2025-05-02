48bc3954cab9d96046a5ae09e7ea8495cdb27d14
Marijanjghhava <marijanjghava.88@gmail.com>
2025-05-03 00:23:04 +0400

    added solutions to some exercise from ocaml.org

--- ocamlorg.ml
+++ ocamlorg.ml
@@ -1,17 +1,54 @@
-(*Tail of a list*)
-
+(*Tail of a List*)
 let rec last list =
   match list with 
-  [] -> None
-  | [x]  -> Some x
-  | h :: t -> last t;; 
+  | [] -> None
+  | [x] -> Some x
+  | h :: t -> last t;;
 
-(* Last two elements of a List*)
- let rec last_two list =
-  match list with 
-  [] -> None
-  | [x] -> None
-  | [x; y] -> Some (x , y)
-  | h :: t -> last_two t;;
+ (*Last Two Elements of a list*)
+  let rec last_two list =
+    match list with 
+    | [] | [_] -> None
+    | [x; y] -> Some (x,y)
+    | h :: t -> last_two t;;
+
+ (*N'th Element of a List*)
+    let rec n'th n list =
+      match list with 
+      | [] -> None
+      | h :: t ->if n =0 then Some h else n'th (n-1) t;;
+
+  (*Length of a List*)
+  let rec length list =
+    match list with 
+    [] -> 0
+    | h :: t -> 1 + length t;;
+
+    (*tail recursive version*)
+    let  length list =
+      let rec aux n = function
+      | [] -> n
+      | h :: t -> aux (n + 1) t
+    in 
+    aux 0 list;;
+
+
+  (*Reverse a List*)
+  let rec reverse list =
+    match list with 
+    [] -> []
+    |h :: t -> reverse t @ [h];;
+
+  (*tail recursive version*)
+  let reverse list =
+    let rec aux acc = function
+      | [] -> acc
+      | h :: t -> aux (h :: acc) t
+    in
+    aux [] list;;
+
+  
+
+ 
 
 
